7292d4669b0e2356ef7c17801c9a6b54c76b05e0
Dummybot <dummybot0505@gmail.com>
2023-10-05 11:27:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,141 @@
+const express = require('express')
+const app = express()
+const { v4: uuidV4 } = require('uuid')
+var data = [{
+   uuid: uuidV4(),
+   name: "Satish",
+   image: "https://source.unsplash.com/random",
+   desc: "Description",
+   like: 30,
+   comment: 13,
+   share: 21,
+   more: {
+       upload_date: Date.now(),
+       upload_time: Date.now(),
+       location: "my home"
+   },
+   comment_people: [
+      {
+        name: "Anomyous says: ",
+        comment: "Awesome pic..."
+      },
+      {
+        name: "Anomyous says: ",
+        comment: "Awesome pic..."
+      },
+      {
+        name: "Anomyous says: ",
+        comment: "Awesome pic..."
+      },
+      {
+        name: "Anomyous says: ",
+        comment: "Awesome pic..."
+      }
+   ]
+},
+{
+   uuid: uuidV4(),
+   name: "Satish",
+   image: "https://source.unsplash.com/random",
+   desc: "Description",
+   like: 30,
+   comment: 13,
+   share: 21,
+   more: {
+       upload_date: Date.now(),
+       upload_time: Date.now(),
+       location: "my home"
+   },
+   comment_people: [
+      {
+        name: "Anomyous says: ",
+        comment: "Awesome pic..."
+      },
+      {
+        name: "Anomyous says: ",
+        comment: "Awesome pic..."
+      },
+      {
+        name: "Anomyous says: ",
+        comment: "Awesome pic..."
+      },
+      {
+        name: "Anomyous says: ",
+        comment: "Awesome pic..."
+      }
+   ]
+},
+{
+   uuid: uuidV4(),
+   name: "Satish",
+   image: "https://source.unsplash.com/random",
+   desc: "Description",
+   like: 30,
+   comment: 13,
+   share: 21,
+   more: {
+       upload_date: Date.now(),
+       upload_time: Date.now(),
+       location: "my home"
+   },
+   comment_people: [
+      {
+        name: "Anomyous says: ",
+        comment: "Awesome pic..."
+      },
+      {
+        name: "Anomyous says: ",
+        comment: "Awesome pic..."
+      },
+      {
+        name: "Anomyous says: ",
+        comment: "Awesome pic..."
+      },
+      {
+        name: "Anomyous says: ",
+        comment: "Awesome pic..."
+      }
+   ]
+},
+{
+   uuid: uuidV4(),
+   name: "Satish",
+   image: "https://source.unsplash.com/random",
+   desc: "Description",
+   like: 30,
+   comment: 13,
+   share: 21,
+   more: {
+       upload_date: Date.now(),
+       upload_time: Date.now(),
+       location: "my home"
+   },
+   comment_people: [
+      {
+        name: "Anomyous says: ",
+        comment: "Awesome pic..."
+      },
+      {
+        name: "Anomyous says: ",
+        comment: "Awesome pic..."
+      },
+      {
+        name: "Anomyous says: ",
+        comment: "Awesome pic..."
+      },
+      {
+        name: "Anomyous says: ",
+        comment: "Awesome pic..."
+      }
+   ]
+}
+];
+
+app.get('/', (req, res) => {
+   res.send(data)
+})
+
+
+app.listen(3000, () => {
+   console.log("Yes")
+})
